4dadc5707585ef732549a0841efeafef484234e3
Ankesh Kumar <ankeshnirala@gmail.com>
2021-03-28 22:54:15 +0530

    auth module testing done with jest and supertest

---
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,90 @@
+import request from "supertest";
+import { app } from "./../../app";
+
+it("returns a 201 on successfull signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "adamfks",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "adamfks",
+      password: "pa",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "adamfks",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email & password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "adamfks",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("sets a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
